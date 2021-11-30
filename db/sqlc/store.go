41daabfa5b3bca7d74f974699aaaac0207848ef7
vm-ubuntu <1873978303@qq.com>
2021-11-30 04:51:33 +0000

    add getAccount api test

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -7,21 +7,27 @@ import (
 )
 
 //Store provide all functions to execute db queries and transactons
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+//SQLStore provide all functions to execute SQL queries and transactons
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
 //NewStore create a new Store
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 //execTx executes a function within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -57,7 +63,7 @@ type TransferTxResult struct {
 
 //TransferTx performs a money transfer from one account to the other
 //It creates a transfer record, add account entries, and update accounts' balance whitin a single database transaction
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
