ae4c1ab812ebec6513a8dc26182b1936756f9181
shui12jiao <mengsiming77@gmail.com>
2023-01-03 11:35:34 +0800

    add swagger

--- go.mod
+++ go.mod
@@ -8,13 +8,20 @@ require (
 	github.com/go-playground/validator/v10 v10.11.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/mock v1.6.0
+	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.3.0
 	github.com/lib/pq v1.10.7
 	github.com/o1egl/paseto v1.0.0
+	github.com/rakyll/statik v0.1.7
 	github.com/spf13/viper v1.14.0
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/crypto v0.4.0
+	google.golang.org/grpc v1.51.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0
+	google.golang.org/protobuf v1.28.1
 )
+
+require github.com/golang/glog v1.0.0 // indirect
 
 require (
 	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
@@ -25,6 +32,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -43,10 +51,10 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/net v0.4.0
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/genproto v0.0.0-20221227171554-f9683d7f8bef
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -3,32 +3,114 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"github.com/shui12jiao/my_simplebank/api"
+	"github.com/shui12jiao/my_simplebank/apig"
 	db "github.com/shui12jiao/my_simplebank/db/sqlc"
+	_ "github.com/shui12jiao/my_simplebank/doc/statik"
+	"github.com/shui12jiao/my_simplebank/pb"
 	"github.com/shui12jiao/my_simplebank/util"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("cannot load config:", err)
+		log.Fatal("failed to load config:", err)
 	}
 	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
-		log.Fatal("cannot connect to db:", err)
+		log.Fatal("failed to connect to db:", err)
 	}
 
 	store := db.NewStore(conn)
+	// go runHTTPServer(config, store)
+	go runGatewayServer(config, store)
+	runGRPCServer(config, store)
+}
+
+func runHTTPServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
-		log.Fatal("cannot create server")
+		log.Fatal("failed to create http server")
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
-		log.Fatal("can not start server:", err)
+		log.Fatal("can not start http server:", err)
+	}
+}
+
+func runGRPCServer(config util.Config, store db.Store) {
+	server, err := apig.NewServer(config, store)
+	if err != nil {
+		log.Fatal("failed to create grpc server")
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("failed to start grpc server:", err)
+	}
+
+	log.Println("starting grpc server at", config.GRPCServerAddress)
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("failed to start grpc server:", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := apig.NewServer(config, store)
+	if err != nil {
+		log.Fatal("failed to create gateway server")
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	grpcMux := runtime.NewServeMux(runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	}))
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("failed to register grpc gateway:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("failed to create statik file system:", err)
+	}
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("failed to start gateway server:", err)
+	}
+
+	log.Println("starting gateway server at", config.HTTPServerAddress)
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("failed to start gateway server:", err)
 	}
 }
